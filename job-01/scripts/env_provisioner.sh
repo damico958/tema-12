373dff23b51b28aa3feb33b45c432c01dfb17a61
damico958 <damico.lipe@gmail.com>
2022-02-15 15:04:56 -0300

    Corrige Tema 12 - Jenkins
    - Adiciona a flag GOOS=linux na hora de construir a aplicação em Go.

--- job-01/scripts/env_provisioner.sh
+++ job-01/scripts/env_provisioner.sh
@@ -4,5 +4,5 @@ cd application || exit
 echo "Installing dependencies..."
 go get -d -v ./.. && go install -v ./..
 echo "Building application..."
-CGO_ENABLED=0 go build -o calculator-app
+CGO_ENABLED=0 GOOS=linux go build -o calculator-app
 pwd 
